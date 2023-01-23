e174554f367afe036c761e8577ff32739876b37c
Dorian Gonzalez <doriandarren1@gmail.com>
2023-01-23 13:25:13 +0100

    add route

--- src/Repositories/ToApis/Helpers/GenerateToApiRoute.php
+++ src/Repositories/ToApis/Helpers/GenerateToApiRoute.php
@@ -39,8 +39,8 @@ class GenerateToApiRoute
         $contents .= HelperFiles::formatLineBreakAndTab('use App\\Http\\Controllers\\' . $classNamePluralUp . '\\' . $classNameSingularUp . EnumFolderToApi::UPDATE . 'Controller;',null,1);
         $contents .= HelperFiles::formatLineBreakAndTab('use App\\Http\\Controllers\\' . $classNamePluralUp . '\\' . $classNameSingularUp . EnumFolderToApi::DESTROY . 'Controller;',null,1);
         $contents .= HelperFiles::formatLineBreakAndTab('use Illuminate\\Support\\Facades\\Route;',null,2);
-
         
+
 
         $contents .= HelperFiles::formatLineBreakAndTab('/**',null,1);
         $contents .= HelperFiles::formatLineBreakAndTab('* '. $classNamePluralUp,null,1);
