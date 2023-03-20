94d21342efb8a554564ebdc8cad886397a52f3c5
Dorian Gonzalez <doriandarren1@gmail.com>
2023-03-20 11:40:02 +0100

    enhance postman

--- src/Repositories/ToApis/Helpers/GenerateToApiRoute.php
+++ src/Repositories/ToApis/Helpers/GenerateToApiRoute.php
@@ -50,7 +50,7 @@ class GenerateToApiRoute
 
 
 
-        $contents .= HelperFiles::formatLineBreakAndTab('Route::group([\'prefix\' => EnumApiSetup::API_VERSION . \''.$tableNameWithGuionPlural.'/\'], function () {',null,2);
+        $contents .= HelperFiles::formatLineBreakAndTab('Route::group([\'prefix\' => EnumApiSetup::API_VERSION . \''. str_replace("_", "-", $tableNameWithGuionPlural) .'/\'], function () {',null,2);
         $contents .= HelperFiles::formatLineBreakAndTab('Route::group([\'middleware\' => \'auth:sanctum\'], function() {',null,2, 1);
 
 
